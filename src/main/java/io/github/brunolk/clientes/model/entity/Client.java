07038ca185f4110e56e0e1ed0f5e25e79f446a76
Bruno Nogueira <brunoncarvalho@outlook.com>
2021-02-06 17:14:41 -0300

    Intenacionalização, encode e algumas validações

--- src/main/java/io/github/brunolk/clientes/model/entity/Client.java
+++ src/main/java/io/github/brunolk/clientes/model/entity/Client.java
@@ -24,12 +24,12 @@ public class Client {
     private Integer id;
 
     @Column(nullable = false, length = 150)
-    @NotEmpty
+    @NotEmpty(message = "{campo.nome.obrigatorio}")
     private String name;
 
     @Column(nullable = false, length = 11)
-    @NotNull
-    @CPF
+    @NotNull(message = "{campo.cpf.obrigatorio}")
+    @CPF(message = "{campo.nome.invalido}")
     private String cpf;
 
     @Column(name = "data_cadastro", updatable = false)
