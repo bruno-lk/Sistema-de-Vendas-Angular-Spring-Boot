cf50b9b0d52acd7f5f6a1c086e094e6455bf1c3d
Bruno Nogueira <brunoncarvalho@outlook.com>
2021-02-11 17:02:10 -0300

    Sidebar base com toggle

--- clientes-app/src/app/app.module.ts
+++ clientes-app/src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './template/navbar/navbar.component';
+import { TemplateModule } from './template/template.module';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, TemplateModule],
   providers: [],
   bootstrap: [AppComponent],
 })
